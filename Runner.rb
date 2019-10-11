3615bbc2bfabb5fa23830ae1b749711719609a02
Addison Grant <addison@ifixit.com>
2019-10-11 12:11:41 -0700

    Use api key (optionally) for private pages

--- Runner.rb
+++ Runner.rb
@@ -1,10 +1,14 @@
+require 'shellwords'
+require 'json'
+
 class LighthouseRunner
    INTERNAL_ROOT = '/var/lighthouse'
 
-   def initialize output_directory, endpoint_name, url
+   def initialize output_directory, endpoint_name, url, api_key = nil
       @output_directory = output_directory
       @endpoint_name = endpoint_name
       @url = url
+      @api_key = api_key
    end
 
    def run
@@ -17,7 +21,8 @@ class LighthouseRunner
          --only-categories=accessibility \
          --chrome-flags='--headless --no-sandbox' \
          --output=json --output=html \
-         --output-path "#{internal_output_path}" \
+         --output-path="#{internal_output_path}" \
+         #{extra_headers} \
          "#{@url}"
       DOCKER_RUN
       log :debug, docker_run_cmd
@@ -32,5 +37,13 @@ class LighthouseRunner
 
    def absolute_output_path
       return File.expand_path(@output_directory)
+   end
+
+   def extra_headers
+      if @api_key then
+        return "--extra-headers=\"{\\\"Authorization\\\": \\\"api #{@api_key}\\\"}\""
+      else
+        return ""
+      end
    end
 end
--- run.rb
+++ run.rb
@@ -8,7 +8,7 @@ args = parse_args <<DOCOPT
 Lighthouse Runner
 
 Usage:
-  #{File.basename __FILE__} [--html] <output_directory> <endpoint_name> <URL>
+  #{File.basename __FILE__} <output_directory> <endpoint_name> <URL> [<api_key>]
 
 Options:
   --html              Generate HTML format report
@@ -20,5 +20,6 @@ log :info, "Running Lighthouse against '#{args["<URL>"]}'"
 output_directory = args['<output_directory>']
 endpoint_name = args['<endpoint_name>']
 url = args['<URL>']
+api_key = args['<api_key>']
 
-LighthouseRunner.new(output_directory, endpoint_name, url).run
+LighthouseRunner.new(output_directory, endpoint_name, url, api_key).run
