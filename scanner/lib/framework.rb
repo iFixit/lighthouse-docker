cbb2ff37ad19c235157a13b83f467920f68e475f
Andrew Gilbert <andrewg@ifixit.com>
2021-06-24 16:57:46 -0700

    Use `unpack_pages` method directly
    Rather than calling it at init time, let's call it when we want a list
    of pages.

--- scanner/lib/framework.rb
+++ scanner/lib/framework.rb
@@ -4,17 +4,15 @@ Page = Struct.new :name, :url
 
 class Framework
   attr_reader :name
-  attr_accessor :pages
 
   def initialize(name, pages, hostname)
     @name = name
-    @_pages = pages
+    @pages = pages
     @hostname = hostname
-    @pages = unpack_pages
   end
 
-  def unpack_pages
-    @_pages.map do |name, url|
+  def pages
+    @pages.map do |name, url|
       uri = URI.parse url
       uri.host = @hostname if @hostname
       Page.new name, uri
