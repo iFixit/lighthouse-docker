c48e4d57c59d913320ad927aa169b8ce5c9ecbb9
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-05-31 10:32:45 -0700

    integrate DataDog client into Lighthouse run

--- lib/lighthouse.py
+++ lib/lighthouse.py
@@ -1,6 +1,8 @@
 import json
 import subprocess
 
+from datadog import DataDogApiClient
+
 class Lighthouse:
     command = ['lighthouse', '--output=json', '--quiet', '--chrome-flags="--headless"']
 
@@ -11,34 +13,44 @@ class Lighthouse:
         if result.returncode != 0:
             raise Exception('Lighthouse failed')
 
-        # save json to a file
-        with open('lighthouse.json', 'w') as f:
-            f.write(result.stdout.decode('utf-8'))
-
         json_results = json.loads(result.stdout)
+        metrics = self.parse_json_report(json_results)
+        self.send_metrics_to_datadog(url, metrics)
+
 
     def parse_json_report(self, json_results):
         # Cumulative Layout Shift
         cumulative_layout_shift = self._get_audits_value(json_results, 'cumulative-layout-shift')
-        print('Cumulative Layout Shift: ', cumulative_layout_shift)
 
         # Largest Contentful Paint
         largest_contentful_paint = self._get_audits_value(json_results, 'largest-contentful-paint')
-        print('Largest Contentful Paint: ', largest_contentful_paint)
 
         # Total Blocking Time
         total_blocking_time = self._get_audits_value(json_results, 'total-blocking-time')
-        print('Total Blocking Time: ', total_blocking_time)
 
         # Network Request Total Transfer Size
         page_size = json_results.get('audits').get('diagnostics').get('details').get('items')[0].get('totalByteWeight')
-        print('Page Size: ', page_size)
+
+        return {
+            'cumulative_layout_shift': cumulative_layout_shift,
+            'largest_contentful_paint': largest_contentful_paint,
+            'total_blocking_time': total_blocking_time,
+            'page_size': page_size
+        }
+
 
     def _get_audits_value(self, json_results, audit_name):
         return json_results.get('audits').get(audit_name).get('numericValue') or 0
 
+    def send_metrics_to_datadog(self, url, metrics):
+        tags = {'url': url, 'test': 'test'}
+        tags = [f'{k}:{v}' for k, v in tags.items()]
+
+        datadog = DataDogApiClient.get_instance()
+
+        for metric_name, value in metrics.items():
+            datadog.submit_metric(f'lighthouse.{metric_name}', value, tags)
+
 if __name__ == '__main__':
     lighthouse = Lighthouse()
-    # lighthouse.run('https://www.ifixit.com')
-    json_results = json.load(open('lighthouse.json'))
-    lighthouse.parse_json_report(json_results)
+    lighthouse.run('https://www.ifixit.com')
