1c404775b0579afef315d52ba883e6fbbaa69b20
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-05-31 10:07:50 -0700

    convert the datadog code into a class and add tags:

--- lib/datadog.py
+++ lib/datadog.py
@@ -1,38 +1,64 @@
 from datadog_api_client.configuration import Configuration
 from datadog_api_client.api_client import ApiClient
-import os, time
 from datadog_api_client.v2.api import metrics_api
 from datadog_api_client.v2.model.metric_point import MetricPoint
 from datadog_api_client.v2.model.metric_series import MetricSeries
 from datadog_api_client.v2.model.metric_payload import MetricPayload
 from datadog_api_client.v2.model.metric_intake_type import MetricIntakeType
-import os
+import os, time
 
 from dotenv import load_dotenv
 load_dotenv()
 
-configuration = Configuration()
-api_client = ApiClient(configuration)
+class DataDogApiClient:
+    __instance = None
 
-configuration.api_key["apiKeyAuth"] = os.getenv('DD_API_KEY')
-configuration.api_key["appKeyAuth"] = os.getenv('DD_APPLICATION_KEY')
+    @staticmethod
+    def get_instance():
+        if DataDogApiClient.__instance == None:
+            return DataDogApiClient()
+        return DataDogApiClient.__instance
 
-from datadog_api_client.v1.model.point import Point
+    def __init__(self):
+        """ Virtually private constructor. """
+        if DataDogApiClient.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            DataDogApiClient.__instance = self
+            configuration = Configuration()
+            api_client = ApiClient(configuration)
 
-metric = MetricSeries(
-    metric="lighthouse.cumulative_layout_shift",
-    points=[
-        MetricPoint(
-            timestamp=int(time.time()),
-            value=0.1
+            configuration.api_key["apiKeyAuth"] = os.getenv('DD_API_KEY')
+            configuration.api_key["appKeyAuth"] = os.getenv('DD_APPLICATION_KEY')
+
+            self.metrics_api_instance = metrics_api.MetricsApi(api_client)
+
+    def submit_metric(self, metric_name, value, tags=[]):
+        metric = MetricSeries(
+            metric=metric_name,
+            points=[
+                MetricPoint(
+                    timestamp=int(time.time()),
+                    value=value
+                )
+            ],
+            type=MetricIntakeType.GAUGE,
+            tags=tags
         )
-    ],
-    type=MetricIntakeType.GAUGE,
-)
 
-metrics_payload = MetricPayload(
-    series=[metric]
-)
+        metrics_payload = MetricPayload(
+            series=[metric]
+        )
 
-metrics_api_instance = metrics_api.MetricsApi(api_client)
-metrics_api_instance.submit_metrics(body=metrics_payload)
+        self.metrics_api_instance.submit_metrics(body=metrics_payload)
+
+
+if __name__ == '__main__':
+    tags = {'url': 'https://www.ifixit.com', 'test': 'test'}
+    tags = [f'{k}:{v}' for k, v in tags.items()]
+
+    datadog = DataDogApiClient.get_instance()
+    datadog.submit_metric('lighthouse.cumulative_layout_shift', 0.5, tags)
+    datadog.submit_metric('lighthouse.largest_contentful_paint', 0.3,  tags)
+    datadog.submit_metric('lighthouse.total_blocking_time', 1.231,  tags)
+    datadog.submit_metric('lighthouse.page_size', 155129,  tags)
