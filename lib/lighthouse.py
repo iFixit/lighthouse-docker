1a1f6d986d5643297d447c8fe31ad291a95e0029
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-06-29 15:52:02 -0700

    update lighthouse py class to use docker container
    The Lighthouse py class will now execute its command through a lighthouse
    docker container instead of directly on the host. This will allow us to
    easily run the python code on any host that has docker installed.
    
    That said, this adds some additional checks to ensure that the docker
    image is available and to print out the results of the subprocess upon
    failure instead of masking it with just an error message.

--- lib/lighthouse.py
+++ lib/lighthouse.py
@@ -2,13 +2,33 @@ import json
 import subprocess
 
 class Lighthouse:
-    command = ['lighthouse', '--output=json', '--quiet', '--chrome-flags="--headless"']
+    IMAGE_NAME = 'lighthouse'
+
+    def __init__(self):
+        # Check if the lighthouse image is locally saved
+        result = subprocess.run(['docker', 'images', self.IMAGE_NAME], stdout=subprocess.PIPE)
+        if result.returncode != 0 or self.IMAGE_NAME not in result.stdout.decode():
+            raise Exception(f'{self.IMAGE_NAME} image not found.\nTry running: `docker build -t {self.IMAGE_NAME} ./lighthouse-docker`')
+
+        self.command = [
+            'docker', 'run',
+            '--rm',
+            '--name', 'lighthouse',
+            self.IMAGE_NAME
+        ]
+
+        self.lighthouse_options = [
+            '--output=json',
+            '--quiet',
+        ]
 
     def run(self, url):
-        command = self.command + [url]
+        command = self.command + [url] + self.lighthouse_options
         result = subprocess.run(command, stdout=subprocess.PIPE)
 
         if result.returncode != 0:
+            output = result.stdout.decode('utf-8')
+            print(output)
             raise Exception('Lighthouse failed')
 
         return json.loads(result.stdout)
