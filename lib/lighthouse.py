7ed6730f15c9de4ddb0046aaed882e2ad40ea199
Angel de la Torre <angel.dlt.1996@gmail.com>
2023-08-14 11:05:43 -0700

    checkpoint: verify if lighthouse npm package is installed

--- lib/lighthouse.py
+++ lib/lighthouse.py
@@ -1,43 +1,52 @@
 import json
 import subprocess
 
+def is_lighthouse_installed_locally():
+    try:
+        result = subprocess.run(['npm', 'list', 'lighthouse'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
+        if 'lighthouse@' in output:
+            print('Lighthouse Installed Locally')
+            return True
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+def is_lighthouse_installed_globally():
+    try:
+        result = subprocess.run(['npm', 'list', '-g', 'lighthouse'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output = result.stdout.decode('utf-8')
+        if 'lighthouse@' in output:
+            print('Lighthouse Installed Globally')
+            return True
+        return False
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return False
+
+
 class Lighthouse:
-    IMAGE_NAME = 'lighthouse'
-
     def __init__(self):
-        # Check if the lighthouse image is locally saved
-        result = subprocess.run(['docker', 'images', self.IMAGE_NAME], stdout=subprocess.PIPE)
-        if result.returncode != 0 or self.IMAGE_NAME not in result.stdout.decode():
-            raise Exception(f'{self.IMAGE_NAME} image not found.\nTry running: `docker build -t {self.IMAGE_NAME} ./lighthouse-docker`')
+        # Check if the lighthouse cli package is installed locally
+        if not is_lighthouse_installed_globally() and not is_lighthouse_installed_locally():
+            raise Exception('Lighthouse package is not installed.\nPlease install it with `npm install -g lighthouse` or `npm install lighthouse`')
 
-        self.command = [
-            'docker', 'run',
-            '--rm',
-            '--name', 'lighthouse',
-            self.IMAGE_NAME
-        ]
+    # def run(self, url, options=[]):
+    #     options = options + self.lighthouse_options
+    #     command = self.command + [url] + options
+    #     result = subprocess.run(command, stdout=subprocess.PIPE)
 
-        self.lighthouse_options = [
-            '--no-enable-error-reporting',
-            '--output=json',
-            '--quiet',
-        ]
+    #     if result.returncode != 0:
+    #         output = result.stdout.decode('utf-8')
+    #         print(output)
+    #         raise Exception('Lighthouse failed')
 
-    def run(self, url, options=[]):
-        options = options + self.lighthouse_options
-        command = self.command + [url] + options
-        result = subprocess.run(command, stdout=subprocess.PIPE)
-
-        if result.returncode != 0:
-            output = result.stdout.decode('utf-8')
-            print(output)
-            raise Exception('Lighthouse failed')
-
-        return json.loads(result.stdout)
+    #     return json.loads(result.stdout)
 
 if __name__ == '__main__':
     lighthouse = Lighthouse()
-    json_result = lighthouse.run('https://www.ifixit.com', ['--preset=desktop'])
-    # Save the json_result to a file for debugging
-    with open('lighthouse.json', 'w') as f:
-        json.dump(json_result, f, indent=4)
+    # json_result = lighthouse.run('https://www.ifixit.com', ['--preset=desktop'])
+    # # Save the json_result to a file for debugging
+    # with open('lighthouse.json', 'w') as f:
+    #     json.dump(json_result, f, indent=4)
